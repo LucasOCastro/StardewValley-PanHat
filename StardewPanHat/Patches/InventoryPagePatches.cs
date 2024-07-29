ebe2036f1e1090f1d42236914931a792d12b53b9
lucas castro <lucas_o__castro@hotmail.com>
2024-07-29 10:54:58 -0300

    Unwrap HatWrapper when removing from attachments

---
+++ StardewPanHat/Patches/InventoryPagePatches.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using StardewValley;
+using StardewValley.Menus;
+
+namespace StardewPanHat.Patches;
+
+public static class InventoryPagePatches
+{
+    private static void setHeldItem_UnwrapHatWrapper_Prefix(ref Item item)
+    {
+        if (item is HatWrapper wrapper)
+        {
+            wrapper.onDetachedFromParent();
+            item = wrapper.InternalHat;
+        }
+    }
+
+    public static void PatchAll(Harmony harmony)
+    {
+        harmony.Patch(
+            original: AccessTools.Method(typeof(InventoryPage), "setHeldItem"),
+            prefix: new(typeof(InventoryPagePatches), nameof(setHeldItem_UnwrapHatWrapper_Prefix))
+        );
+    }
+}
