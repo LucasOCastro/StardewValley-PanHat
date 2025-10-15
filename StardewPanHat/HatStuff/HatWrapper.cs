bd5bc0e5d369abbd49a06e6bcaa9d04dde09594a
lucas castro <lucas_o__castro@hotmail.com>
2025-10-14 23:21:41 -0300

    Uses hat id constant instead of string

--- StardewPanHat/HatStuff/HatWrapper.cs
+++ StardewPanHat/HatStuff/HatWrapper.cs
@@ -1,9 +1,10 @@
 ﻿using System.Xml.Serialization;
 using Netcode;
+using StardewValley;
 using StardewValley.Objects;
 using Object = StardewValley.Object;
 
-namespace StardewPanHat;
+namespace StardewPanHat.HatStuff;
 
 [XmlType($"Mods_{ModEntry.ModAuthorName}_{nameof(HatWrapper)}")]
 public class HatWrapper : Object
@@ -12,9 +13,7 @@ public class HatWrapper : Object
     private readonly NetRef<Hat> _internalHat = new();
     public Hat InternalHat => _internalHat.Value;
 
-    public override string DisplayName => InternalHat.DisplayName;
-
-    public override string TypeDefinitionId => "(H)";
+    public override string TypeDefinitionId => ItemRegistry.type_hat;
 
     public HatWrapper()
     {
