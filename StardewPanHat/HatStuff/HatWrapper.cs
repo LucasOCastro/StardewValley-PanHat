cd6232475d7e16539c04691a3834a9d7323f2aa7
lucas castro <lucas_o__castro@hotmail.com>
2025-10-15 00:42:38 -0300

    Fixes private NetRef

--- StardewPanHat/HatStuff/HatWrapper.cs
+++ StardewPanHat/HatStuff/HatWrapper.cs
@@ -9,25 +9,25 @@ namespace StardewPanHat.HatStuff;
 [XmlType($"Mods_{ModEntry.ModAuthorName}_{nameof(HatWrapper)}")]
 public class HatWrapper : Object
 {
-    [XmlElement(nameof(_internalHat))]
-    private readonly NetRef<Hat> _internalHat = new();
-    public Hat InternalHat => _internalHat.Value;
+    [XmlElement(nameof(InternalHatNetRef))]
+    public readonly NetRef<Hat> InternalHatNetRef = new();
+    
+    public Hat InternalHat => InternalHatNetRef.Value;
 
     public override string TypeDefinitionId => ItemRegistry.type_hat;
 
     public HatWrapper()
     {
-        _internalHat.Value = new();
     }
 
     public HatWrapper(Hat hat) : base(hat.ItemId, 1)
     {
-        _internalHat.Value = hat;
+        InternalHatNetRef.Value = hat;
     }
 
     protected override void initNetFields()
     {
         base.initNetFields();
-        NetFields.AddField(_internalHat, nameof(_internalHat));
+        NetFields.AddField(InternalHatNetRef, nameof(InternalHatNetRef));
     }
 }
